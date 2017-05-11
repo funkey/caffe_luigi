b8d7dd7cc8bdf6f4b116f22bb220e1ed60efe5dd
Jan Funke <jfunke@iri.upc.edu>
2017-05-11 09:21:03 -0400

    Add more merge functions to config.py

--- config.py
+++ config.py
@@ -10,8 +10,15 @@ if waterz.__version__ == '0.6':
     }
 else:
     scoring_function = {
+            'mean_aff': 'OneMinus<MeanAffinity<RegionGraphType, ScoreValue>>',
+            '15_aff': 'OneMinus<QuantileAffinity<RegionGraphType, 15, ScoreValue>>',
+            '15_aff_histograms': 'OneMinus<HistogramQuantileAffinity<RegionGraphType, 15, ScoreValue, 256>>',
+            '25_aff': 'OneMinus<QuantileAffinity<RegionGraphType, 25, ScoreValue>>',
+            '25_aff_histograms': 'OneMinus<HistogramQuantileAffinity<RegionGraphType, 25, ScoreValue, 256>>',
             'median_aff': 'OneMinus<QuantileAffinity<RegionGraphType, 50, ScoreValue>>',
             'median_aff_histograms': 'OneMinus<HistogramQuantileAffinity<RegionGraphType, 50, ScoreValue, 256>>',
+            '75_aff': 'OneMinus<QuantileAffinity<RegionGraphType, 75, ScoreValue>>',
+            '75_aff_histograms': 'OneMinus<HistogramQuantileAffinity<RegionGraphType, 75, ScoreValue, 256>>',
             '85_aff': 'OneMinus<QuantileAffinity<RegionGraphType, 85, ScoreValue>>',
             '85_aff_histograms': 'OneMinus<HistogramQuantileAffinity<RegionGraphType, 85, ScoreValue, 256>>',
             'max_10': 'OneMinus<MeanMaxKAffinity<RegionGraphType, 10, ScoreValue>>'
