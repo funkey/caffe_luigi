058a44babf422d1b7633f498f9b9ea40d759b1d5
Jan Funke <jfunke@iri.upc.edu>
2017-06-07 09:10:08 -0400

    Reduce CPU requirements to 1 for Train and Process
    This is a lie, but it ensures that all tasks are placed as long as GPUs
    are available.

--- tasks.py
+++ tasks.py
@@ -54,7 +54,7 @@ class TrainTask(luigi.Task):
         os.chdir(os.path.join(base_dir, '02_train', self.setup))
         check_call([
             'run_mesos.sh',
-            '-c', '10',
+            '-c', '1', # a shameless lie, just to get tasks running
             '-g', '1',
             '-d', 'funkey/gunpowder:v0.2-prerelease',
             '-e', 'python -u train_until.py ' + str(self.iteration) + ' 0 1>%s 2>%s'%(log_out,log_err)
@@ -86,7 +86,7 @@ class ProcessTask(luigi.Task):
         os.chdir(os.path.join(base_dir, '03_process'))
         check_call([
             'run_mesos.sh',
-            '-c', '5',
+            '-c', '1',
             '-g', '1',
             '-d', 'funkey/gunpowder:v0.2-prerelease',
             '-e', 'python -u predict_affinities.py ' + self.setup + ' ' + str(self.iteration) + ' ' + self.sample + ' 0 1>%s 2>%s'%(log_out,log_err)
